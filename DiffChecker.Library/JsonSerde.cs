7440f95b0b80c937f41701948bef998ef973aa02
Tadeas Kozub <tedkozub@github.com>
2024-11-22 13:35:25 +0100

    extract json serialization and deserialization into static class

---
+++ DiffChecker.Library/JsonSerde.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace DiffChecker;
+
+public static class JsonSerde
+{
+    private const string JsonSaveFilePath = @"C:\Users\teddy\Documents\analysis.json"; // TODO: extract to config
+
+    public static bool CheckIfJsonExists()
+    {
+        return File.Exists(JsonSaveFilePath);
+    }
+
+    public static DirectoryNode LoadJson()
+    {
+        var json = File.ReadAllText(JsonSaveFilePath);
+        return JsonConvert.DeserializeObject<DirectoryNode>(json);
+    }
+
+    public static void SaveJson(DirectoryNode nodes)
+    {
+        var json = JsonConvert.SerializeObject(nodes);
+        File.WriteAllText(JsonSaveFilePath, json);
+    }
+}
