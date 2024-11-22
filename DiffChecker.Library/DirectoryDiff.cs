a77d8f39bef1197902344061e1bb51abd5798542
Tadeas Kozub <tedkozub@github.com>
2024-11-22 13:34:48 +0100

    introduce new classes for creating diff

---
+++ DiffChecker.Library/DirectoryDiff.cs
@@ -0,0 +1,12 @@
+﻿namespace DiffChecker;
+
+public class DirectoryDiff
+{
+    public List<FileNode> AddedFiles { get; set; } = [];
+    public List<FileNode> ModifiedFiles { get; set; } = [];
+    public List<FileNode> DeletedFiles { get; set; } = [];
+
+    public List<DirectoryNode> AddedDirectories { get; set; } = [];
+    public List<DirectoryNode> DeletedDirectories { get; set; } = [];
+    public List<DirectoryDiff> SubdirectoryDiffs { get; set; } = [];
+}
---
+++ DiffChecker.Library/DirectoryNode.cs
@@ -0,0 +1,9 @@
+﻿namespace DiffChecker;
+
+public class DirectoryNode
+{
+    public string Name { get; set; }
+    public string Path { get; set; }
+    public List<FileNode> Files { get; set; } = [];
+    public List<DirectoryNode> Directories { get; set; } = [];
+}
